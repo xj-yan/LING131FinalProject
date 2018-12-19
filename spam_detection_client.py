236f632324315b2cb21dca4bc4284c7d461bd7fa
xj-yan <xyan@brandeis.edu>
2018-12-19 10:01:36 -0500

    Update spam_detection_client.py

--- spam_detection_client.py
+++ spam_detection_client.py
@@ -1,11 +1,20 @@
 import string
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
-from analysis_sklearn import ngram_char_features, create_labeled_data, tokenize_labeled_data
+from sklearn.feature_extraction.text import CountVectorizer
+from analysis_sklearn import ngram_char_features
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
-stop_words = set(stopwords.words('english'))
+# stop_words = set(stopwords.words('english'))
 
-
+def ngram_char_features_text(text):
+    #calculate the ngram of characters within a word bound, 
+    #parameter n give the number of ngram. calculate exact that number of gram, 
+    #means not including ngrams less than n.
+    ngram_vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(5, 5))
+    text_transformed=ngram_vectorizer.fit_transform(text)                            
+    return text_transformed
 
 def run_MLP_Classifier(feature, text):
     X, Y=feature(data)
@@ -20,11 +29,12 @@ if __name__ == '__main__':
 
 	print("This is a spam detector")
 	print("You can input received msg to test whether this is a spam")
-	test = input("Please enter you message: ")
-	translator = str.maketrans('', '', string.punctuation)
-	test = test.translate(translator)
-	print(test)
-	word_token = test.split(' ')
-	stop_words = set(stopwords.words('english'))
-	print(type(word_token))
-	print(word_token)
+	text = input("Please enter you message: ")
+	text = [text]
+	# translator = str.maketrans('', '', string.punctuation)
+	# test = test.translate(translator)
+	# print(test)
+	# word_token = test.split(' ')
+	# stop_words = set(stopwords.words('english'))
+	text = ngram_char_features_text(text)
+	run_MLP_Classifier(ngram_char_features, text)
