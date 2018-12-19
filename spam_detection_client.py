9ba5b3833bdcfb250116a68c03105823c3ae94f7
xj-yan <xyan@brandeis.edu>
2018-12-19 00:31:13 -0500

    Update spam_detection_client.py

--- spam_detection_client.py
+++ spam_detection_client.py
@@ -1,4 +1,13 @@
+import string
+from nltk.corpus import stopwords
+
 print("This is a spam detection")
 print("You can input received msg to test whether this is a spam")
 test = input("Please enter you message: ")
+translator = str.maketrans('', '', string.punctuation)
+test = test.translate(translator)
 print(test)
+word_token = test.split(' ')
+stop_words = set(stopwords.words('english'))
+print(type(word_token))
+print(word_token)
