77e5be65928306f551f508bc4fcc370f14cc3863
洪叶 <osora27@qq.com>
2018-12-18 20:27:23 -0500

    rename

---
+++ analysis_models.py
@@ -0,0 +1,125 @@
+from models.NeuralNetwork import NeuralNetwork
+from models.NaiveBayes import NaiveBayes
+from sklearn import preprocessing
+import numpy as np
+import copy
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+# feature extraction based on analysis_sklearn.py
+
+# 1. raw count
+def raw_count_feature(X):
+    count_vectorizer = CountVectorizer(decode_error='ignore')
+    X = count_vectorizer.fit_transform(X)
+    return X.toarray()
+
+# 2. tfidf
+def tfidf_feature(X):
+    tfidf_vectorizer = TfidfVectorizer(decode_error='ignore')
+    X = tfidf_vectorizer.fit_transform(X)
+    return X.toarray()
+#
+# # 3. bigram
+# def bigram_feature(X):
+#
+
+# run NaiveBayes classifier
+def run_naivebayes_classifier(x_train, y_train, x_test, y_test):
+    nb_classifier = NaiveBayes(copy.copy(x_train), copy.copy(y_train))
+    nb_classifier.train()
+    print('Self-built NaiveBayes Accuracy with Raw Count: ', nb_classifier.accuracy(copy.copy(x_test), copy.copy(y_test)))
+
+
+# run NeuralNetwork classifier
+def run_neuralnetwork_classifier(x_train, y_train, x_test, y_test, hidden_layer_size, iteration_count):
+    nn_classifier = NeuralNetwork(copy.copy(x_train), copy.copy(y_train), hidden_layer_size)
+    nn_classifier.train(iteration_count)
+    print('Self-built NeuralNetwork Accuracy with Raw Count: ', nn_classifier.accuracy(copy.copy(x_test), copy.copy(y_test)))
+
+
+# read raw data and label from file
+def read_from_file(file_name):
+    data = pd.read_csv(file_name, encoding = "ISO-8859-1")
+    raw_text = list(data['v2'])
+    label = list(data['v1'])
+    return raw_text, label
+
+# split the training set and the test set by a percentage
+def train_test_split(X, Y, percentage):
+    length = X.shape[0]
+    split = int(length * percentage)
+    X_train = X[0: split]
+    y_train = Y[0: split]
+    X_test = X[split + 1:]
+    y_test = Y[split + 1:]
+    return X_train, y_train, X_test, y_test
+
+def standardize_y(Y):
+    y = []
+    for word in Y:
+        if word == 'spam':
+            y.append([1])
+        else:
+            y.append([0])
+    return np.array(y)
+
+def main():
+    # read from file
+    X, Y = read_from_file('spam.csv')
+    # standardize y
+    y = standardize_y(Y)
+    # 1. Raw Count
+    print('Feature: Raw Count')
+    # Step 1: feature extraction
+    x1 = raw_count_feature(copy.copy(X))
+    # Step 2: feature scaling
+    # x1 = preprocessing.scale(x1)
+    # Step 3: divide into training and test set
+    x_train, y_train, x_test, y_test = train_test_split(x1, y, 0.9)
+    # Step 4: run classifiers
+    #     Neural Network: 20 neurons in hidden layer; 1000 iterations
+    #     Result: iteration time 2000: 86.71454219030521
+    run_neuralnetwork_classifier(x_train, y_train, x_test, y_test, 20, 2000)
+    #     Naive Bayes
+    #     Result: 86.983842010772
+    run_naivebayes_classifier(x_train, y_train, x_test, y_test)
+
+    # 2. TF-IDF
+    print('Feature: TF-IDF')
+    # Step 1: feature extraction
+    x2 = tfidf_feature(copy.deepcopy(X))
+    # Step 2: feature scaling
+    x2 = preprocessing.scale(x2) # feature scaling
+    # Step 3: divide into training and test set
+    x_train, y_train, x_test, y_test = train_test_split(x2, y, 0.9)
+
+    # Step 4: run classifiers
+    #     Neural Network: 20 neurons in hidden layer
+    #     Result: iteration time 5000: 86.71454219030521
+    run_neuralnetwork_classifier(x_train, y_train, x_test, y_test, 20, 2000)
+    #     Naive Bayes
+    #     Result: 87.07360861759426
+    run_naivebayes_classifier(x_train, y_train, x_test, y_test)
+
+
+    #
+    # # 3. Most frequent 200 words
+    # print('Feature: Most frequent 200 words')
+    # # Step 1: feature extraction
+    # x3 = bigram_feature(copy.deepcopy(X))
+    # # Step 2: feature scaling
+    # x3 = preprocessing.scale(x3) # feature scaling
+    # # Step 3: divide into training and test set
+    # x_train, y_train, x_test, y_test = train_test_split(x3, y, 0.9)
+    # # Step 4: run classifiers
+    # #     Neural Network: 4 neurons in hidden layer
+    # #     Result: iteration time :
+    # run_neuralnetwork_classifier(x_train, y_train, x_test, y_test, 4)
+    # #     Naive Bayes
+    # #     Result:
+    # run_naivebayes_classifier(x_train, y_train, x_test, y_test)
+
+
+if __name__ == '__main__':
+    main()
--- models/SpaceVector.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-from AbstractClassifier import AbstractClassifier 
-
-class SpaceVector(object):
-	"""docstring for SpaceVector"""
-	def __init__(self, arg):
-		super(SpaceVector, self).__init__()
-		self.arg = arg
-		
